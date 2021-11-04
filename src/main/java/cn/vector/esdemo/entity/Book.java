f7d82e734ad4ada7523b65c52b747a723f3e607c
lshan <50941420+Lshan523@users.noreply.github.com>
2021-11-04 11:29:38 +0800

    Update Book.java
    add remark

--- src/main/java/cn/vector/esdemo/entity/Book.java
+++ src/main/java/cn/vector/esdemo/entity/Book.java
@@ -14,7 +14,14 @@ import java.util.List;
  * <p>
  * 如果不使用@Field注解指定数据类型的话
  * Spring Data Elasticsearch框架会自动映射数据类型，如果es服务器中不存在索引的话会自动创建
+ *  @Field(type=FieldType.Text, analyzer="ik_max_word")     表示该字段是一个文本，并作最大程度拆分，默认建立索引 
+ *  @Field(type=FieldType.Text,index=false)             表示该字段是一个文本，不建立索引
+ *  @Field(type=FieldType.Date)                                表示该字段是一个文本，日期类型，默认不建立索引
+ *  @Field(type=FieldType.Long)                               表示该字段是一个长整型，默认建立索引
+ *  @Field(type=FieldType.Keyword)                         表示该字段内容是一个文本并作为一个整体不可分，默认建立索引
+ *  @Field(type=FieldType.Float)                               表示该字段内容是一个浮点类型并作为一个整体不可分，默认建立索引
  */
+
 @Document(indexName = "book")
 public class Book {
 
